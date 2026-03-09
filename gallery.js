a1ab926a52b60161924c24b1c81ebc1aaaac39ce
jmeinhof <jonathan@meinhof.de>
2026-03-09 11:14:56 +0100

    gallery separated

---
+++ gallery.js
@@ -0,0 +1,23 @@
+const galleryItems = [
+  {
+    title: "Happy Focusing!",
+    thumb: "gallery/Happy_focusing.jpg",
+    image: "gallery/Happy_focusing.jpg",
+    alt: "Happy Focusing playlist cover",
+    description: "Cover art of one of my main playlists: Happy Focusing! This is my oldest but also one of my most impactful playlists, reaching more than 600 monthly listeners. My girlfriend took this picture when we travelled to Bali, Indonesia, in 2018."
+  },
+  {
+    title: "Old Studio",
+    thumb: "gallery/Old_studio.jpeg",
+    image: "gallery/Old_studio.jpeg",
+    alt: "My old home studio in Düsseldorf, Germany",
+    description: "My former home studio desk in the beginning of 2023. I lived in Düsseldorf, Germany, back then."
+  },
+  {
+    title: "Artist Logo",
+    thumb: "gallery/This_is_denada_Cover.png",
+    image: "gallery/This_is_denada_Cover.png",
+    alt: "My denada artist logo",
+    description: "Originally, this had been the cover art for my Colors EP. It soon became my artist logo as well. I actually drew this picture myself on my iPad."
+  }
+];
