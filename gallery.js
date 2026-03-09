836bd9354263021138fe987cc0e39eb51f8424b1
jmeinhof <34664222+jmeinhof@users.noreply.github.com>
2026-03-09 11:50:38 +0100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,10 +1,24 @@
 const galleryItems = [
   {
-    title: "Happy Focusing!",
-    thumb: "gallery/Happy_focusing.jpg",
-    image: "gallery/Happy_focusing.jpg",
-    alt: "Happy Focusing playlist cover",
-    description: "Cover art of one of my main playlists: Happy Focusing! This is my oldest but also one of my most impactful playlists, reaching more than 600 monthly listeners. My girlfriend took this picture when we travelled to Bali, Indonesia, in 2018."
+    title: "Winter and Spring Cover",
+    thumb: "gallery/Winterandspring_cover.jpg",
+    image: "gallery/Winterandspring_cover.jpg",
+    alt: "Winter and Spring Cover",
+    description: "Winter and Spring (Feb 2026) was my first collab with Resonant Circuits. He created the visuals and came up with this beautiful cover art."
+  },
+  {
+    title: "Spotlights Cover",
+    thumb: "gallery/Spotlights_cover.jpg",
+    image: "gallery/Jade_cover.jpg",
+    alt: "Spotlights Cover",
+    description: "Spotlights (Dec 2025): This is what happens if you have a song in the upload process but forgot to come up with a cover. I quickly went onto my balcony, moved my phone around, and randomly took pictures. So it was a quick and dirty process, but I was quite happy with the result."
+  },
+  {
+    title: "Jade Cover",
+    thumb: "gallery/Jade_logo.jpeg",
+    image: "gallery/Jade_logo.jpeg",
+    alt: "Jade Cover",
+    description: "Cover picture of my song Jade (January 2025). This has been the first release of my dreamier alter ego second thoughts."
   },
   {
     title: "Old Studio",
@@ -14,10 +28,31 @@ const galleryItems = [
     description: "My former home studio desk in the beginning of 2023. I lived in Düsseldorf, Germany, back then."
   },
   {
+    title: "Indigo Cover",
+    thumb: "gallery/Indigo_logo.jpeg",
+    image: "gallery/Indigo_logo.jpeg",
+    alt: "Indigo Cover",
+    description: "Cover of my single Indigo (released in June 2024). The song is definitely one of my personal favorits. It was also the first time ever I used AI to generate (parts of) the cover image. Shortly after, I decided to not continue using AI images furthermore. Using my own photos instead for covers is definitely more work, but I hope they give my releases a more personal, also more human touch."
+  },
+  {
     title: "Artist Logo",
     thumb: "gallery/This_is_denada_Cover.png",
     image: "gallery/This_is_denada_Cover.png",
     alt: "My denada artist logo",
     description: "Originally, this had been the cover art for my Colors EP. It soon became my artist logo as well. I actually drew this picture myself on my iPad."
-  }
+  },
+  {
+    title: "Breathe Cover",
+    thumb: "gallery/Breathe_logo.jpg",
+    image: "gallery/Breathe_logo.jpg",
+    alt: "Breathe Cover",
+    description: "A bench at the west coast of Sweden. I took this picture during my summer holidays in 2021. It became the cover image of my song Breathe. The song really is something different from my usual style. I thought about taking it down several times, because it might be too different, but I still like it too much for that."
+  },
+  {
+    title: "Happy Focusing!",
+    thumb: "gallery/Happy_focusing.jpg",
+    image: "gallery/Happy_focusing.jpg",
+    alt: "Happy Focusing playlist cover",
+    description: "Cover art of one of my main playlists: Happy Focusing! This is my oldest but also one of my most impactful playlists, reaching more than 600 monthly listeners. My girlfriend took this picture when we travelled to Bali, Indonesia, in 2018."
+  }  
 ];
