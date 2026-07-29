fba1e8164b838616839b509c50ddbf8ddce042d9
jmeinhof <jonathan@meinhof.de>
2026-07-29 21:41:46 +0200

    fixes, impressum, media compression

--- gallery.js
+++ gallery.js
@@ -1,57 +1,65 @@
 const galleryItems = [
   {
     title: "Winter and Spring",
-    thumb: "gallery/Winterandspring_cover.jpg",
-    image: "gallery/Winterandspring_cover.jpg",
+    thumbSmall: "gallery/thumbs/320/Winterandspring_cover.webp",
+    thumb: "gallery/thumbs/Winterandspring_cover.webp",
+    image: "gallery/Winterandspring_cover.webp",
     alt: "Winter and Spring Cover",
     description: "Winter and Spring (Feb 2026) was my first collab with Resonant Circuits. He created the visuals and came up with this beautiful cover art."
   },
   {
     title: "Spotlights",
-    thumb: "gallery/Spotlights_cover.jpg",
-    image: "gallery/Spotlights_cover.jpg",
+    thumbSmall: "gallery/thumbs/320/Spotlights_cover.webp",
+    thumb: "gallery/thumbs/Spotlights_cover.webp",
+    image: "gallery/Spotlights_cover.webp",
     alt: "Spotlights Cover",
     description: "Spotlights (Dec 2025): This is what happens if you have a song in the upload process but forgot to come up with a cover. I quickly went onto my balcony, moved my phone around, and randomly took pictures. So it was a quick and dirty process, but I was quite happy with the result."
   },
   {
     title: "Jade",
-    thumb: "gallery/Jade_logo.jpeg",
-    image: "gallery/Jade_logo.jpeg",
+    thumbSmall: "gallery/thumbs/320/Jade_logo.webp",
+    thumb: "gallery/thumbs/Jade_logo.webp",
+    image: "gallery/Jade_logo.webp",
     alt: "Jade Cover",
     description: "Cover picture of my song Jade (January 2025). This has been the first release of my dreamier alter ego second thoughts."
   },
   {
     title: "Former Homestudio",
-    thumb: "gallery/Old_studio.jpeg",
-    image: "gallery/Old_studio.jpeg",
+    thumbSmall: "gallery/thumbs/320/Old_studio.webp",
+    thumb: "gallery/thumbs/Old_studio.webp",
+    image: "gallery/Old_studio.webp",
     alt: "My old home studio in Düsseldorf, Germany",
     description: "My former home studio desk in the beginning of 2023. I lived in Düsseldorf, Germany, back then."
   },
   {
     title: "Indigo",
-    thumb: "gallery/Indigo_logo.jpeg",
-    image: "gallery/Indigo_logo.jpeg",
+    thumbSmall: "gallery/thumbs/320/Indigo_logo.webp",
+    thumb: "gallery/thumbs/Indigo_logo.webp",
+    image: "gallery/Indigo_logo.webp",
     alt: "Indigo Cover",
-    description: "Cover of my single Indigo (released in June 2024). The song is definitely one of my personal favorits. It was also the first time ever I used AI to generate (parts of) the cover image. Shortly after, I decided to not continue using AI images furthermore. Using my own photos instead for covers is definitely more work, but I hope they give my releases a more personal, also more human touch."
+    description: "Cover of my single Indigo (released in June 2024). The song is definitely one of my personal favorites. It was also the first time I used AI to generate parts of a cover image. Shortly afterwards, I decided not to continue using AI images. Using my own photos for covers is more work, but I hope they give my releases a more personal and human touch."
   },
   {
     title: "Artist Logo",
-    thumb: "gallery/This_is_denada_Cover.png",
-    image: "gallery/This_is_denada_Cover.png",
+    thumbSmall: "gallery/thumbs/320/This_is_denada_Cover.webp",
+    thumb: "gallery/thumbs/This_is_denada_Cover.webp",
+    image: "gallery/This_is_denada_Cover.webp",
     alt: "My denada artist logo",
     description: "Originally, this had been the cover art for my Colors EP. It soon became my artist logo as well. I actually drew this picture myself on my iPad."
   },
   {
     title: "Breathe",
-    thumb: "gallery/Breathe_logo.jpg",
-    image: "gallery/Breathe_logo.jpg",
+    thumbSmall: "gallery/thumbs/320/Breathe_logo.webp",
+    thumb: "gallery/thumbs/Breathe_logo.webp",
+    image: "gallery/Breathe_logo.webp",
     alt: "Breathe Cover",
     description: "A bench at the west coast of Sweden. I took this picture during my summer holidays in 2021. It became the cover image of my song Breathe. The song really is something different from my usual style. I thought about taking it down several times, because it might be too different, but I still like it too much for that."
   },
   {
     title: "Happy Focusing!",
-    thumb: "gallery/Happy_focusing.jpg",
-    image: "gallery/Happy_focusing.jpg",
+    thumbSmall: "gallery/thumbs/320/Happy_focusing.webp",
+    thumb: "gallery/thumbs/Happy_focusing.webp",
+    image: "gallery/Happy_focusing.webp",
     alt: "Happy Focusing playlist cover",
     description: "Cover art of one of my main playlists: Happy Focusing! This is my oldest but also one of my most impactful playlists, reaching more than 600 monthly listeners. My girlfriend took this picture when we travelled to Bali, Indonesia, in 2018."
   }  
